aba4d4bace0e0bc08ef6998cd9191bab941c10de
tom_glb <goodMoring_glb@xhf.com>
2021-06-20 11:53:13 +0800

    第一次提交

---
+++ src/main/java/com/xu/GitTests.java
@@ -0,0 +1,11 @@
+package com.xu;
+
+/**
+ * @author: 许洪峰
+ * @create: 2021-06-20 11:49
+ **/
+public class GitTests {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
